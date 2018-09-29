9346fd00b5cdc2182af04ffd511c059b196967dc
ink <jiangink@foxmail.com>
2018-09-29 12:51:03 +0800

    add migration file

--- typings/app/service/index.d.ts
+++ typings/app/service/index.d.ts
@@ -2,14 +2,14 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg'; // Make sure ts to import egg declaration at first
-import Test from '../../../app/service/Test';
 import JwtAuth from '../../../app/service/jwtAuth';
+import Test from '../../../app/service/Test';
 import User from '../../../app/service/user';
 
 declare module 'egg' {
   interface IService {
-    test: Test;
     jwtAuth: JwtAuth;
+    test: Test;
     user: User;
   }
 }
